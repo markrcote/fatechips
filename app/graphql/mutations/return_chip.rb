88d38ae3c2149c89ea69dde22dbb59da65f9204b
Mark Côté <mark.cote@uwaterloo.ca>
2020-09-13 16:31:52 -0400

    Add return_chip GraphQL mutation.

---
+++ app/graphql/mutations/return_chip.rb
@@ -0,0 +1,20 @@
+module Mutations
+  class ReturnChip < BaseMutation
+    argument(:game_id, ID, required: true)
+    argument(:chip_type, String, required: true)
+
+    field(:chip_count, Types::ChipCountType, null: false)
+    field(:game, Types::GameType, null: false)
+    field(:errors, [String], null: false)
+
+    def resolve(game_id:, chip_type:)
+      game = Game.find(game_id)
+      chip_count = game.return_chip(chip_type: chip_type)
+      {
+        chip_count: chip_count,
+        game: game,
+        errors: []
+      }
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field(:take_chip, mutation: Mutations::TakeChip)
+    field(:return_chip, mutation: Mutations::ReturnChip)
   end
 end
