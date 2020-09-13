070ea2301186580128da3412ef3559da496e9de2
Mark Côté <mark.cote@uwaterloo.ca>
2020-09-13 16:22:25 -0400

    Add Game.return_chip method.

--- app/models/game.rb
+++ app/models/game.rb
@@ -23,6 +23,23 @@ class Game < ApplicationRecord
     return chip_count
   end
 
+  def return_chip(chip_type:)
+    chip_count = nil
+    chip_pool.chip_count.each do |pool_chip_count|
+      if pool_chip_count.chip_type == chip_type
+        chip_count = pool_chip_count
+        break
+      end
+    end
+
+    if !chip_count.nil?
+      chip_count.count += 1
+      chip_count.save
+    end
+
+    chip_count
+  end
+
   def chips
     chip_pool.chip_count.all
   end
