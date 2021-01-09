13361a43c56e26a35cd2515d37af304e9c34a18f
Mark Côté <mark.cote@uwaterloo.ca>
2021-01-08 22:05:38 -0500

    Bump to Rails 6.1.1.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_19_185913) do
+ActiveRecord::Schema.define(version: 2021_01_09_030224) do
 
   create_table "chip_counts", force: :cascade do |t|
     t.integer "chip_type"
