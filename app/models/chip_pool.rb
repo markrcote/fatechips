c4848db08e97525e1c673311e2d2d2b3de356def
Mark Côté <mark.cote@uwaterloo.ca>
2021-01-28 09:55:11 -0500

    Create a ChipCount if necessary when taking chips.
    Also destroy associated players when a game is destroyed.

--- app/models/chip_pool.rb
+++ app/models/chip_pool.rb
@@ -1,23 +1,35 @@
 class ChipPool < ApplicationRecord
   has_many :chip_count, dependent: :destroy
 
-  def add_chips(chip_type, count=1)
+  def change_chip_count(chip_type, count)
     returned_chip_count = find_chip_count(chip_type)
 
-    if !returned_chip_count.nil?
-      if count < 0 and returned_chip_count.count < count * -1
-        return nil
-      end
-
-      returned_chip_count.count += count
+    if returned_chip_count.nil?
+      returned_chip_count = ChipCount.create(
+        count: 0,
+        chip_type: chip_type,
+        chip_pool_id: id
+      )
       returned_chip_count.save
     end
 
+    # Make sure there are enough chips in the pool if we
+    # are taking them away.
+    if count < 0 and returned_chip_count.count < count * -1
+      return nil
+    end
+
+    returned_chip_count.count += count
+    returned_chip_count.save
     returned_chip_count
   end
 
+  def add_chips(chip_type, count=1)
+    return change_chip_count(chip_type, count)
+  end
+
   def remove_chips(chip_type, count=1)
-    return add_chips(chip_type, count * -1)
+    return change_chip_count(chip_type, count * -1)
   end
 
   def take_random_chip
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
   include GraphQL::Interface
 
-  belongs_to :chip_pool
-  has_many :player
+  belongs_to :chip_pool, dependent: :destroy
+  has_many :player, dependent: :destroy
   validates :name, presence: true
 end
